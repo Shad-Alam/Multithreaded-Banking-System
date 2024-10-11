aa0130b61a303faadd1a4671aea89008f770197a
Shad-Alam <shad.19bd@gmail.com>
2024-10-11 15:46:54 +0600

    remove bank account and doc and update

--- Banking-System/src/BankSystem.java
+++ Banking-System/src/BankSystem.java
@@ -1,18 +1,5 @@
 import java.util.ArrayList;
 
-/***
-Initialize Accounts
-○ Create instances of Bank Account with initial balances.
-        ● Create and Manage Threads
-○ Create instances of Transaction Thread with different operations.
-        ○ Use a thread pool or manage threads manually to execute
-operations.
-● Collect and Display Results
-○ Print the final balances of all accounts after all threads have finished
-executing.
-        Note: These are suggestions, you can improvise the system if required.
- */
-
 public class BankSystem {
 
     // search index using student ID
@@ -27,9 +14,8 @@ public class BankSystem {
     }
     public static void main(String[] args) {
         ArrayList<BankAccount> bankAccounts = new ArrayList<BankAccount>();
-        bankAccounts.add(new BankAccount("Masruk Ahmed Ruhani","2019000000019", "Account_001", 157500));
-        bankAccounts.add(new BankAccount("Shad Alam Joy","2019000000059", "Account_003", 126000));
-
+        bankAccounts.add(new BankAccount("Shad Joy","2029000000034", "Account_003", 126000));
+        bankAccounts.add(new BankAccount("Md. Rahat","2029000000037", "Account_004", 156000));
 
         BankAccount bankAccount = new BankAccount();
         String studentID = "2019000000059";

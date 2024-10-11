250dc703ec4316dec1a2043eac398d9652a24868
Shad-Alam <shad.19bd@gmail.com>
2024-10-11 15:57:01 +0600

    update doc

--- Banking-System/src/BankSystem.java
+++ Banking-System/src/BankSystem.java
@@ -1,5 +1,17 @@
 import java.util.ArrayList;
 
+/***
+Initialize Accounts
+○ Create instances of Bank Account with initial balances.
+        ● Create and Manage Threads
+○ Create instances of Transaction Thread with different operations.
+        ○ Use a thread pool or manage threads manually to execute
+operations.
+● Collect and Display Results
+○ Print the final balances of all accounts after all threads have finished
+executing.
+ */
+
 public class BankSystem {
 
     // search index using student ID
